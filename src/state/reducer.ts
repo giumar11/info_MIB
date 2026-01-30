31329c1a0d4061f69838710f96b5b21a7420319c
giumar11 <70144363+giumar11@users.noreply.github.com>
2026-01-30 22:11:40 +0100

    Delete src directory

--- src/state/reducer.ts
+++
@@ -1,9 +0,0 @@
-export function reducer(state, action) {
-  const draft = { ...state };
-  switch (action.type) {
-    case "UPDATE_USERNAME":
-      draft.username = action.payload;
-      break;
-  }
-  return draft;
-}
